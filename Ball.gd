ee145734f8499fb41889b7b9274aa847dcf30cd6
Jacek Jakubik <jacek.jakubik@netguru.pl>
2021-04-22 12:38:08 +0200

    Something not right

--- Ball.gd
+++ Ball.gd
@@ -1,5 +1,7 @@
 extends RigidBody2D
 
-func _integrate_forces(state):
-  if Input.is_action_just_pressed("ui_cancel"):
-    state.transform = Transform2D((get_viewport_rect().size / 2), (get_viewport_rect().size / 2), Vector2.ZERO)
+func _on_Ball_body_entered(body):
+  print("!")
+
+func _process(delta):
+  print('hello')
--- Player.gd
+++ Player.gd
@@ -1,43 +1,45 @@
 extends KinematicBody2D
 
-const RUN_SPEED = 200
+const RUN_SPEED = 220
 var velocity = Vector2.ZERO
 const GRAVITY = 600
 const JUMP_SPEED = -400
 var jumping = false
-var inertia = 200
 
 onready var animationPlayer = $AnimationPlayer
 onready var animationTree = $AnimationTree
 onready var animationState = animationTree.get("parameters/playback")
 
 func _physics_process(delta):
-  if !jumping:
-    velocity.x = 0
+  velocity.x = 0
     
   var right = Input.is_action_pressed('ui_right')
   var left = Input.is_action_pressed('ui_left')
   var jump = Input.is_action_just_pressed("ui_up")
     
-  if right and !jumping:
-    velocity.x += RUN_SPEED
-    animationState.travel("RunRight")
+  if right:
+    velocity.x = RUN_SPEED
+    $Sprite.flip_h = false
+    if !jumping:
+      animationState.travel("RunRight")
 
-  if left and !jumping:
-    velocity.x -= RUN_SPEED
-    animationState.travel("RunLeft")
+  if left:
+    velocity.x = -RUN_SPEED
+    $Sprite.flip_h = true
+    if !jumping:
+      animationState.travel("RunLeft")
 
   if !right && !left && !jumping:
     animationState.travel("Idle")
 
   velocity.y += GRAVITY * delta;
   
-  velocity = move_and_slide(velocity, Vector2.UP, false, 4, PI/4, false)
-
   if jump and is_on_floor():
     jumping = true
     velocity.y = JUMP_SPEED
     animationState.travel("JumpStart")
+    
+  velocity = move_and_slide(velocity, Vector2.UP, false, 4, PI/4, false)
 
   if jumping and is_on_floor():
     jumping = false
