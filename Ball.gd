f47ca70449122628a1abaee895cdf7b2462d4970
Jacek Jakubik <jacek.jakubik@netguru.pl>
2021-04-22 16:02:11 +0200

    Floor + net

--- Ball.gd
+++ Ball.gd
@@ -2,6 +2,3 @@ extends RigidBody2D
 
 func _on_Ball_body_entered(body):
   print("!")
-
-func _process(delta):
-  print('hello')
--- Player.gd
+++ Player.gd
@@ -12,11 +12,11 @@ onready var animationState = animationTree.get("parameters/playback")
 
 func _physics_process(delta):
   velocity.x = 0
-    
+
   var right = Input.is_action_pressed('ui_right')
   var left = Input.is_action_pressed('ui_left')
   var jump = Input.is_action_just_pressed("ui_up")
-    
+
   if right:
     velocity.x = RUN_SPEED
     $Sprite.flip_h = false
@@ -33,12 +33,12 @@ func _physics_process(delta):
     animationState.travel("Idle")
 
   velocity.y += GRAVITY * delta;
-  
+
   if jump and is_on_floor():
     jumping = true
     velocity.y = JUMP_SPEED
     animationState.travel("JumpStart")
-    
+
   velocity = move_and_slide(velocity, Vector2.UP, false, 4, PI/4, false)
 
   if jumping and is_on_floor():
